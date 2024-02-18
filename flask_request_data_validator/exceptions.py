1eb8f5db400a2fdef083e39f5f8c6a6af49c1b8e
ooknimm <ooknimm@gmail.com>
2024-02-18 23:56:07 +0900

    change compatible typing for 3.9

--- flask_request_data_validator/exceptions.py
+++ flask_request_data_validator/exceptions.py
@@ -1,10 +1,10 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
 from pydantic_core import ErrorDetails
 
 
 class RequestValidationError(Exception):
-    def __init__(self, errors: List[Dict[str, Any] | ErrorDetails]) -> None:
+    def __init__(self, errors: List[Union[Dict[str, Any], ErrorDetails]]) -> None:
         super().__init__()
         self.errors = errors
 
